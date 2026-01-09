d89e8403f502e6518509ecd81af37266b0dfbbc5
Janany <janany7024@gmail.com>
2026-01-09 10:17:45 +0530

    login.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Login from './auth/Login';
 import MemberDashboard from './pages/member/MemberDashboard';
 
 function App() {
   return (
     <div>
       <MemberDashboard />
+      
     </div>
   );
 }
